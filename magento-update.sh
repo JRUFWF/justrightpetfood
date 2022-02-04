4b8c3c1644b922109a6d59697065825d069d7a81
JRUFWF <wenjun.fan@purina.nestle.com>
2022-02-04 14:38:53 -0800

    bash to rebuild when magento switch to latest branch

---
+++ magento-update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+################ 1: Restart no sync
+echo "********* 1/5 Restart no sync ********* "
+bin/stop "$@"
+bin/start-no-sync "$@"
+
+################ 2: Copy file from host to container
+echo "********* 2/5 Copy file from host to container  ********* "
+bin/copytocontainer --all
+
+################ 3: start container
+echo "******** 3/5 Start container ********* "
+bin/restart
+
+################ 4: sync master data
+echo "********* 4/5 Sync magento master data  ********* "
+bin/sync-master-data
+
+################ 5: build again
+echo "******** 5/5 Composer install and build ********* "
+bin/composer build-and-test
+
+
+
